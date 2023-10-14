e76496dca378deb3cca94b3002f146765d6e7300
ioioio0719 <kio0719@docomo.ne.jp>
2023-10-14 09:10:54 +0900

    add: トップページ、ログアウト機能作成

---
+++ frontend/src/views/contents/Top.vue
@@ -0,0 +1,32 @@
+<script setup>
+import axios from "axios";
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const signOut = () => {
+  axios
+    .delete('/auth/sign_out',
+    {
+      headers: {
+        uid: localStorage.getItem('uid'),
+        'access-token': localStorage.getItem('access-token'),
+        client: localStorage.getItem('client')
+      }
+    })
+    .then((response) =>{
+      localStorage.removeItem('uid');
+      localStorage.removeItem('acces-token');
+      localStorage.removeItem('client');
+      router.push({ name: "login" })
+    })
+};
+
+</script>
+<template>
+  <div>
+    <h1>トップ</h1>
+      <button type="button" @click="signOut">ログアウト</button>
+  </div>
+</template>
+
